212b2d6faea8e56d42fce41ae6979b6bcd87b353
meissnerM224 <meissnermw@gmail.com>
2026-09-25 21:35:04 +0200

    Scaffold React frontend with Vite and pnpm

---
+++ web/src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+   
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  );
+}
+
+export default App;
